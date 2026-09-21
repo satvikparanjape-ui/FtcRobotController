cb0e44612eb53a1aa9bf6cc1b3acd44e0e72d858
satvikparanjape-ui <satvik.paranjape@gmail.com>
2026-09-21 00:42:39 -0500

    Create GamePadPractice.java
    tutorial for working with gamepad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@TeleOp
+public class GamePadPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        // runs 50x+ a second
+        double speedForward = -gamepad.left_Stick / 2.0;
+        double xDiff = Math.abs(gamepad1.left_stick_x - gamepad1.right_stick_x);
+        double sumTriggers = gamepad1.left_trigger + gamepad1.right_trigger;
+
+        telemetry.addData("left x", gamepad1.left_stick_x);
+        telemetry.addData("left y", speedForward);
+        telemetry.addData("right x", gamepad1.right_stick_x);
+        telemetry.addData("right y", gamepad1.right_stick_y);
+        telemetry.addData("difference x", xDiff);
+
+        telemetry.addData("a button", gamepad1.a);
+        telemetry.addData("b button", gamepad1.b);
+
+        telemetry.addData("sum triggers", sumTriggers);
+
+    }
+}
