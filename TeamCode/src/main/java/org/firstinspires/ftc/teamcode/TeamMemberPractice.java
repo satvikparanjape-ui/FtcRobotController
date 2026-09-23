b609ceecce6e4a5a1322c03de1d3704c9bb1b274
satvikparanjape-ui <satvik.paranjape@gmail.com>
2026-09-22 21:57:11 -0500

    Practice with not-void-variables
    learned about special variables and how to use and define functions. Also learned how to manipulate variables.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPractice.java
@@ -7,9 +7,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class TeamMemberPractice extends OpMode {
 
     boolean initDone;
-    
+
     @Override
     public void init() {
+        telemetry.addData("init", initDone);
+        initDone = true;
+
         /*
         double x;
 
@@ -21,8 +24,25 @@ public class TeamMemberPractice extends OpMode {
         */
     }
 
+    double squareInputWithSign(double input) {
+        double output = input * input;
+
+        if (input < 0) {
+            output *= -1;
+        }
+        return output;
+    }
+
     @Override
     public void loop() {
+        telemetry.addData("init", initDone);
 
+        double yAxis = gamepad1.left_stick_y;
+
+        telemetry.addData("Left Stick Normal", yAxis);
+
+        yAxis = squareInputWithSign(yAxis);
+
+        telemetry.addData("Left Stick Modified", yAxis);
     }
 }
