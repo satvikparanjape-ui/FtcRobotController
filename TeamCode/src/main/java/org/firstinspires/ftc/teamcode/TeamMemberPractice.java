3b4b54a661c86bafacbefe7c05724e9b6cc3820a
satvikparanjape-ui <satvik.paranjape@gmail.com>
2026-09-22 21:09:54 -0500

    Create TeamMemberPractice.java
    started some practice about writing clean code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamMemberPractice.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TeamMemberPractice extends OpMode {
+
+    boolean initDone;
+    
+    @Override
+    public void init() {
+        /*
+        double x;
+
+        if (x > 0.5) {
+            double y = 10;
+        }
+
+        double sum = x + y; //<-y is not a global variable
+        */
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
